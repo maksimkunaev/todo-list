27c462bf8f215a513860f871d3559e332d3a4df5
Maxim Kunaev <>
2019-10-29 14:17:30 +0300

    add sw

---
+++ serviceWorker.js
@@ -0,0 +1,29 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/main.css',
+    '/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed 🚀🚀🚀');
+
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch 🚀🚀🚀', e.request.url);
+
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
